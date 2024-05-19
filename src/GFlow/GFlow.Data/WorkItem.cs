85c55d14e0fdb7c2d13d33e78d67739afc8d45f1
Martin A <lynxestudio@outlook.com>
2024-05-19 01:35:57 -0600

    Adds source file

---
+++ src/GFlow/GFlow.Data/WorkItem.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace GFlow.Data
+{
+public class WorkItem
+{
+	public int Id{set;get;}
+	public string Numref {set;get;}
+	public string Title {set;get;}
+	public string AssignedTo {set;get;}
+	public string Area{set;get;}
+	public string Reason{set;get;}
+	public DateTime Created {set;get;}
+}
+}
+
